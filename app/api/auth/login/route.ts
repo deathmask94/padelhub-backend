277bde24fbabbc53e5e85e4343ebbd4b9e53498c
felimartinezl <fel.martinez.lagos@gmail.com>
2026-05-24 17:48:34 -0400

    cambio de middleware a json para ajustarlo a vercel

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -60,3 +60,13 @@ export async function POST(request: Request) {
     );
   }
 }
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin":  "*",
+      "Access-Control-Allow-Methods": "POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type,Authorization",
+    },
+  });
+}
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -142,3 +142,13 @@ export async function DELETE(request: Request) {
     );
   }
 }
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin":  "*",
+      "Access-Control-Allow-Methods": "POST,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type,Authorization",
+    },
+  });
+}
